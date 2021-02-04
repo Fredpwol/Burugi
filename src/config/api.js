d9834c8e5d894f3ea76663e9502982861b28bf51
Fredpwol <cyborg290@gmail.com>
2021-02-04 14:15:02 +0100

    first commit

---
+++ src/config/api.js
@@ -0,0 +1,10 @@
+export const API = 'https://wc2.rnlab.io';
+
+export const CONSUMER_KEY = 'ck_38f79bb0931e74632168af8a7819cd8b3fef8367';
+export const CONSUMER_SECRET = 'cs_5219cb259c63fd38bd8658104943cac9d1f237c1';
+
+export default {
+  API_ENDPOINT: API,
+  CONSUMER_KEY,
+  CONSUMER_SECRET,
+};
