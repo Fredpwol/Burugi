b1da5fe9a667a16e19259060a53d529b5d3a5f4d
Fredpwol <cyborg290@gmail.com>
2021-02-05 15:57:51 +0100

    update ui

--- src/screens/wishlist.js
+++ src/screens/wishlist.js
@@ -2,14 +2,21 @@ import React from 'react';
 import {connect} from 'react-redux';
 import {withTranslation} from 'react-i18next';
 import isEqual from 'lodash/isEqual';
-import {StyleSheet, View, ActivityIndicator, I18nManager, Image, Dimensions} from 'react-native';
+import {
+  StyleSheet,
+  View,
+  ActivityIndicator,
+  I18nManager,
+  Image,
+  Dimensions,
+} from 'react-native';
 import {Header, ThemedView, Text} from 'src/components';
 import {SwipeListView} from 'react-native-swipe-list-view';
 import ProductItem from 'src/containers/ProductItem';
 import {TextHeader, CartIcon} from 'src/containers/HeaderComponent';
 import Empty from 'src/containers/Empty';
 import ButtonSwiper from 'src/containers/ButtonSwiper';
-import Icon from "react-native-vector-icons/FontAwesome";
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 import {removeWishList} from 'src/modules/common/actions';
 import {fetchWishList} from 'src/modules/product/actions';
@@ -18,12 +25,16 @@ import {
   dataWishListSelector,
 } from 'src/modules/product/selectors';
 
-import {wishListSelector, countWishListSelector} from 'src/modules/common/selectors';
+import {
+  wishListSelector,
+  countWishListSelector,
+} from 'src/modules/common/selectors';
 
 import {margin} from 'src/components/config/spacing';
 import {homeTabs} from 'src/config/navigator';
+import { FlatList } from 'react-native-gesture-handler';
 
-const {width, height} = Dimensions.get("window")
+const {width, height} = Dimensions.get('window');
 class WishListScreen extends React.Component {
   componentDidMount() {
     this.fetchData();
@@ -50,40 +61,72 @@ class WishListScreen extends React.Component {
     if (!data || data.length < 1) {
       return (
         <>
-          <Image source={require("src/assets/images/wishlist.png")} style={{width, height:height/2}} />
-          <View style={{alignItems:"center"}}>
-          <Text h2 bold>{"Your Wishlist Is Empty"}</Text>
-          <Text style={{textAlign: "center"}} >{"Simply sign in to pick up where you left off.\n Go Shopping"}</Text>
-          <Image source={require("src/assets/images/curve-arrow.png")} style={{height: 40, width:60, right:60, top:80, position:"absolute"}} />
-          {/* <View style={styles.fab}> */}
-          {/* </View> */}
+          <Image
+            source={require('src/assets/images/wishlist.png')}
+            style={{width, height: height / 2}}
+          />
+          <View style={{alignItems: 'center'}}>
+            <Text h2 bold>
+              {'Your Wishlist Is Empty'}
+            </Text>
+            <Text style={{textAlign: 'center'}}>
+              {'Simply sign in to pick up where you left off.\n Go Shopping'}
+            </Text>
+            <Image
+              source={require('src/assets/images/curve-arrow.png')}
+              style={{
+                height: 40,
+                width: 60,
+                right: 60,
+                top: 80,
+                position: 'absolute',
+              }}
+            />
+            {/* <View style={styles.fab}> */}
+            {/* </View> */}
           </View>
-          <Icon name="plus-circle" style={styles.fab} onPress={() => navigation.navigate(homeTabs.shop)} />
+          <Icon
+            name="plus-circle"
+            style={styles.fab}
+            onPress={() => navigation.navigate(homeTabs.shop)}
+          />
         </>
       );
     }
     return (
-      <SwipeListView
-        useFlatList
-        keyExtractor={(item) => `${item.id}`}
-        data={data}
-        renderItem={({item, index}) => (
+      <FlatList 
+      data={data} 
+      numColumns={2}
+      keyExtractor={(item) => `${item.id}`}
+      renderItem={({item, index}) => (
           <ProductItem
-            item={item}
-            style={index === 0 ? styles.firstItem : undefined}
-            type="wishlist"
-          />
-        )}
-        renderHiddenItem={({item}) => (
-          <View style={styles.viewSwiper}>
-            <ButtonSwiper onPress={() => this.removeItem(item.id)} />
-          </View>
-        )}
-        leftOpenValue={70}
-        rightOpenValue={-70}
-        disableLeftSwipe={I18nManager.isRTL}
-        disableRightSwipe={!I18nManager.isRTL}
-      />
+          item={item}
+          style={index === 0 ? styles.firstItem : undefined}
+          type="wishlist"
+          onDelete={this.removeItem}
+        />
+      )} />
+      // <SwipeListView
+      //   useFlatList
+      //   keyExtractor={(item) => `${item.id}`}
+      //   data={data}
+        // renderItem={({item, index}) => (
+        //   <ProductItem
+        //     item={item}
+        //     style={index === 0 ? styles.firstItem : undefined}
+        //     type="wishlist"
+        //   />
+        // )}
+      //   renderHiddenItem={({item}) => (
+      //     <View style={styles.viewSwiper}>
+      //       <ButtonSwiper onPress={() => this.removeItem(item.id)} />
+      //     </View>
+      //   )}
+      //   leftOpenValue={70}
+      //   rightOpenValue={-70}
+      //   disableLeftSwipe={I18nManager.isRTL}
+      //   disableRightSwipe={!I18nManager.isRTL}
+      // />
     );
   };
 
@@ -131,12 +174,12 @@ const styles = StyleSheet.create({
   firstItem: {
     borderTopWidth: 1,
   },
-  fab:{
-    fontSize:60,
-    right:10,
-    position:"absolute",
-    bottom: 10
-  }
+  fab: {
+    fontSize: 60,
+    right: 10,
+    position: 'absolute',
+    bottom: 10,
+  },
 });
 
 const mapStateToProps = (state) => ({
