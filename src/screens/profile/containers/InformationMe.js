944044cdc71b7bda8e8710e1b8cef0ce12f079cc
Fredpwol <cyborg290@gmail.com>
2021-02-08 09:53:49 +0100

    removed styles

--- src/screens/profile/containers/InformationMe.js
+++ src/screens/profile/containers/InformationMe.js
@@ -24,7 +24,7 @@ const InformationMe = ({isLogin, clickPage}) => {
         title={t('common:text_account')}
         type="underline"
         titleProps={titleProps}
-        titleStyle={{color:"black"}}
+       
         chevron
         pad={padding.large}
         onPress={() => clickPage(mainStack.account)}
@@ -35,7 +35,7 @@ const InformationMe = ({isLogin, clickPage}) => {
         type="underline"
         titleProps={titleProps}
         chevron
-        titleStyle={{color:"black"}}
+       
         pad={padding.large}
         onPress={() => clickPage(mainStack.order_list)}
       />
@@ -45,7 +45,7 @@ const InformationMe = ({isLogin, clickPage}) => {
         type="underline"
         titleProps={titleProps}
         chevron
-        titleStyle={{color:"black"}}
+       
         pad={padding.large}
         containerStyle={styles.itemEnd}
         onPress={() => clickPage(mainStack.downloads)}
